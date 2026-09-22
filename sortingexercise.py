040cb730ef0652f06787e7eba4be5a8ed29ef0b4
enyazano <enyamrgrtlozano@gmail.com>
2026-09-22 22:10:03 +0800

    edits

--- sortingexercise.py
+++ sortingexercise.py
@@ -78,15 +78,13 @@ def SelectionSortDesc(l: list) -> list:
     n = len(l)
 
     for i in range(n):
-        # Assume the current position holds the largest remaining value
         max_idx = i
 
-        # Scan the unsorted remainder for something bigger
+        # Check the unsorted for a bigger value
         for j in range(i + 1, n):
             if l[j] > l[max_idx]:
                 max_idx = j
 
-        # Place the largest found into position i
         l[i], l[max_idx] = l[max_idx], l[i]
 
     return l
