a3c0692f4c686856dfde12e7e42115f210bc9a19
ruwiii12 <reignara.parico12@gmail.com>
2026-09-22 14:29:56 +0800

    pasted the template to working file

---
+++ sortingexercise.py
@@ -0,0 +1,63 @@
+# Problem 2a
+# 
+# Input: List l of integers
+# Output: Sorted list of integers
+#
+# Requirements:
+# - Implement Bubble Sort (Optimized)
+#   (Optimized = No checking of guaranteed sorted elements)
+# - Implement 2 versions of the algorithm: one that sorts in ascending order and another in descending order
+
+def BubbleSortOptimizedDesc(l: list) -> list:
+    pass # Your code here
+
+
+def BubbleSortOptimizedAsc(l: list) -> list:
+    pass # Your code here
+
+# Problem 2b
+# 
+# Input: List l of integers
+# Output: Sorted list of integers
+# 
+# Requirements:
+# - Implement Insertion Sort
+# - Implement 2 versions of the algorithm: one that sorts in ascending order and another in descending order
+
+def InsertionSortDesc(l: list) -> list:
+    pass # Your code here
+
+
+def InsertionSortAsc(l: list) -> list:
+    pass # Your code here
+
+# Problem 2c
+# 
+# Input: List l of integers
+# Output: Sorted list of integers
+# 
+# Requirements:
+# - Implement Selection Sort
+# - Implement 2 versions of the algorithm: one that sorts in ascending order and another in descending order
+
+def SelectionSortDesc(l: list) -> list:
+    pass # Your code here
+
+
+def SelectionSortAsc(l: list) -> list:
+    pass # Your code here
+
+# Problem 2d
+# 
+# Input: None; The function asks for three user inputs: String list, String sort, and String direction
+#        String list should be only integers separated by a comma and possibly a space (e.g. "1, 2, 3, 4, 5" or "1,2,3,4,5")
+#        String sort should only be "bubble", "insertion", or "selection"
+#        String direction should either be "asc" or "desc"
+# Output: Sorted list of integers (in the desired order)
+# 
+# Requirements:
+# - Use the functions you made in problems 2a - 2c
+# - Handle invalid inputs
+
+def Sorter():
+    pass # Your code here
